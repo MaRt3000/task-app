18900a90b29767949743458c1a4deb4136725939
MaRt <ogbemart@gmail.com>
2026-04-02 13:25:58 +0100

    created delete, patch and post APIs

--- index.js
+++ index.js
@@ -37,6 +37,40 @@ app.post('/api/v1/tasks', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const { title, description, completed } = req.body;
+    const task = tasks.find(t => t.id === parseInt(id));
+    if (!task) {
+        return res.status(404).json({
+            message: 'Task not found'
+        });
+    }
+    if (title) task.title = title;
+    if (description) task.description = description;
+    if (completed !== undefined) task.completed = completed;
+    res.json({
+        message: 'Task updated successfully!',
+        data: {
+            task
+        }
+    });
+});
+
+app.delete('/api/v1/tasks/:id', (req, res) => {
+    const { id } = req.params;
+    const taskIndex = tasks.findIndex(t => t.id === parseInt(id));
+    if (taskIndex === -1) {
+        return res.status(404).json({
+            message: 'Task not found'
+        });
+    }
+    tasks.splice(taskIndex, 1);
+    res.json({
+        message: 'Task deleted successfully!'
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 }); 
